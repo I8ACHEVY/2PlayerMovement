a0c46ae6f1b71d131f10fde61011536af7c3e98d
Kyle <159157285+I8ACHEVY@users.noreply.github.com>
2024-08-22 19:12:16 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,14 +2,56 @@ require("debug")
 
 print("hello world")
 
--- function love.load()
+function love.load()
+    P1x = 100;
+    P1y = 100;
+    P2x = 100;
+    P2y = 100;
+    moveSpeed = 100;
+    move = true;
+end;
 
--- -- end
+function love.draw()
+    love.graphics.setColor(0, 200, 300);
+    -- player 1
+    love.graphics.circle("line", P1x, P1y, 50, 50);
 
--- -- function love.update() --deltaTime
+    -- player 2
+    love.graphics.rectangle("line", P2x, P2y, 200, 150);
+end;
 
--- -- end
+function love.update(dt)
+    -- Moving Player 1
+    if love.keyboard.isDown("right") then
+        P1x = P1x + moveSpeed * dt;
+    elseif love.keyboard.isDown("left") then
+        P1x = P1x - moveSpeed * dt;
+    else
+        print("Invalid arrow key!");
+    end;
 
--- -- function love.draw()
+    if love.keyboard.isDown("up") then
+        P1y = P1y - moveSpeed * dt;
+    elseif love.keyboard.isDown("down") then
+        P1y = P1y + moveSpeed * dt;
+    else
+        print("Invalid arrow key!");
+    end;
 
--- -- end
+    -- Moving Player 2
+    if love.keyboard.isDown("d") then
+        P2x = P2x + moveSpeed * dt;
+    elseif love.keyboard.isDown("a") then
+        P2x = P2x - moveSpeed * dt;
+    else
+        print("Invalid arrow key!");
+    end;
+
+    if love.keyboard.isDown("w") then
+        P2y = P2y - moveSpeed * dt;
+    elseif love.keyboard.isDown("s") then
+        P2y = P2y + moveSpeed * dt;
+    else
+        print("Invalid arrow key!");
+    end;
+end;
